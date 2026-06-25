88f906c43e74b55f207475978d8374c41707b5d8
David Villarreal <wvillar85769@universidadean.edu.co>
2026-06-25 18:30:00 -0500

    Sorteo fecha 5

--- app/team-draw/page.tsx
+++ app/team-draw/page.tsx
@@ -14,41 +14,56 @@ interface Player {
 	puntos: number
 }
 
-const tittle = "SORTEO - FECHA 4"
+const tittle = "SORTEO - FECHA FINAL"
 
 const players: Player[] = [
-	{ id: 1, equipo: 'negro', 
-		nombre: 'Jhon Guzman ⭐', puntos: 0 },
-	{ id: 2, equipo: 'blanco', 
-		nombre: 'Walter Gomez', puntos: 55 },
-	{ id: 3, equipo: 'negro', 
-		nombre: 'Reinel Capera', puntos: 10 },
-	{ id: 4, equipo: 'blanco', 
-		nombre: 'Mauricio Pulido ⭐', puntos: 25 },
-	{ id: 5, equipo: 'negro', 
-		nombre: 'Jean Ramos ⭐', puntos: 51 },
-	{ id: 6, equipo: 'blanco', 
-		nombre: 'Brian Maldonado ⭐', puntos: 0 },
-	{ id: 7, equipo: 'negro', 
-		nombre: 'David Villarreal ⭐', puntos: 29 },
-	{ id: 8, equipo: 'blanco', 
-		nombre: 'Daniel Saavedra', puntos: 46 },
-	{ id: 9, equipo: 'negro', 
-		nombre: 'Juan Martínez ⭐', puntos: 15 },
-	{ id: 10, equipo: 'blanco', 
-		nombre: 'Deison Cagua ⭐', puntos: 10 },
-	{ id: 11, equipo: 'negro', 
-		nombre: 'Keny Quemba ⭐', puntos: 50 },
-	{ id: 12, equipo: 'blanco', 
-		nombre: 'Jeisson Linares ⭐', puntos: 0 },
-	{ id: 13, equipo: 'negro', 
-		nombre: 'Camilo Rincon', puntos: 29 },
-	{ id: 14, equipo: 'blanco', 
-		nombre: 'Alejo Colmenares ⭐', puntos: 40 },
-	{ id: 15, equipo: 'negro', 
-		nombre: 'Jhonatan Molina ⭐', puntos: 0 },
-	{ id: 16, equipo: 'blanco', 
-		nombre: 'Samuel Solarte ⭐', puntos: 15 },
+	{ id: 1, equipo: 'negro',
+		nombre: 'Jean Ramos ⭐', puntos: 74 },
+
+	{ id: 2, equipo: 'blanco',
+		nombre: 'Walter Gomez ⭐', puntos: 65 },
+
+	{ id: 3, equipo: 'negro',
+		nombre: 'Juan Tovar ⭐', puntos: 0 },
+
+	{ id: 4, equipo: 'blanco',
+		nombre: 'Camilo Camargo', puntos: 26 },
+
+	{ id: 5, equipo: 'negro',
+		nombre: 'Mauricio Amaya ⭐', puntos: 10 },
+
+	{ id: 6, equipo: 'blanco',
+		nombre: 'Keny Quemba ⭐', puntos: 65 },
+
+	{ id: 7, equipo: 'negro',
+		nombre: 'David Villarreal ⭐', puntos: 50 },
+
+	{ id: 8, equipo: 'blanco',
+		nombre: 'Jeisson Linares', puntos: 10 },
+
+	{ id: 9, equipo: 'negro',
+		nombre: 'Jefferson Linares', puntos: 0 },
+
+	{ id: 10, equipo: 'blanco',
+		nombre: 'Felipe Baquero ⭐', puntos: 0 },
+
+	{ id: 11, equipo: 'negro',
+		nombre: 'Reinel Capera', puntos: 25 },
+
+	{ id: 12, equipo: 'blanco',
+		nombre: 'Mauricio Pulido (Marang)', puntos: 38 },
+
+	{ id: 13, equipo: 'negro',
+		nombre: 'Camilo Rincon', puntos: 57 },
+
+	{ id: 14, equipo: 'blanco',
+		nombre: 'Alejo Colmenares', puntos: 50 },
+
+	{ id: 15, equipo: 'negro',
+		nombre: 'Jhon Guzman', puntos: 15 },
+
+	{ id: 16, equipo: 'blanco',
+		nombre: 'Referido Walter', puntos: 0 },
 ]
 
 export default function SorteoPage() {
