db1584b974f41ac83611077c3aa45915a6796320
David Villarreal <wvillar85769@universidadean.edu.co>
2026-08-13 19:51:43 -0500

    Tercera fecha

--- app/team-draw/data.ts
+++ app/team-draw/data.ts
@@ -6,55 +6,55 @@ export interface Player {
 	mvp?: boolean
 }
 
-export const MATCH_ID = 'fecha-2'
-export const tittle = "SORTEO - SEGUNDA FECHA"
+export const MATCH_ID = 'fecha-3'
+export const tittle = "SORTEO - TERCERA FECHA"
 
 export const players: Player[] = [
-	{ id: 1, equipo: 'negro',
-		nombre: 'Alexander Rodriguez', puntos: 0 },
+	{ id: 1, equipo: 'negro', 
+		nombre: 'Mauricio Pulido⭐', puntos: 21, mvp: true },
 
-	{ id: 2, equipo: 'blanco',
-		nombre: 'Keny Quemba', puntos: 11 },
+	{ id: 2, equipo: 'blanco', 
+		nombre: 'Sebastian Patiño', puntos: 0 },
 
-	{ id: 3, equipo: 'negro',
-		nombre: 'Walter Gomez', puntos: 7 },
+	{ id: 3, equipo: 'negro', 
+		nombre: 'David Villarreal', puntos: 18 },
 
-	{ id: 4, equipo: 'blanco',
-		nombre: 'Alejandro Colmenares', puntos: 11 },
-		
-	{ id: 5, equipo: 'negro',
-		nombre: 'Nicolás Chacón', puntos: 0 },
+	{ id: 4, equipo: 'blanco', 
+		nombre: 'Fabio Garcia', puntos: 17 },
 
-	{ id: 6, equipo: 'blanco',
-		nombre: 'Jhankarlo Gómez ⭐', puntos: 10, mvp: true },
+	{ id: 5, equipo: 'negro', 
+		nombre: 'Freddy', puntos: 0 },
 
-	{ id: 7, equipo: 'negro',
-		nombre: 'Jean Ramos', puntos: 7 },
+	{ id: 6, equipo: 'blanco', 
+		nombre: 'Jean Ramos', puntos: 16 },
 
-	{ id: 8, equipo: 'blanco',
-		nombre: 'Javier', puntos: 0 },
+	{ id: 7, equipo: 'negro', 
+		nombre: 'Juan David', puntos: 0 },
 
-	{ id: 9, equipo: 'negro',
-		nombre: 'Daniel Saavedra', puntos: 7 },
+	{ id: 8, equipo: 'blanco', 
+		nombre: 'Keny Quemba', puntos: 19 },
 
-	{ id: 10, equipo: 'blanco',
-		nombre: 'Alex Quiroga', puntos: 0 },
+	{ id: 9, equipo: 'negro', 
+		nombre: 'Jhon Guzman', puntos: 13 },
 
-	{ id: 11, equipo: 'negro',
-		nombre: 'Mauricio Pulido', puntos: 9 },
+	{ id: 10, equipo: 'blanco', 
+		nombre: 'Daniel Saavedra', puntos: 16 },
 
-	{ id: 12, equipo: 'blanco',
-		nombre: 'Andrés Gómez', puntos: 7 },
+	{ id: 11, equipo: 'negro', 
+		nombre: 'Alejandro Colmenares', puntos: 12 },
 
-	{ id: 13, equipo: 'negro',
-		nombre: 'Reinel Capera', puntos: 0 },
+	{ id: 12, equipo: 'blanco', 
+		nombre: 'Gabriel Arias', puntos: 0 },
 
-	{ id: 14, equipo: 'blanco',
-		nombre: 'Jeisson Linares', puntos: 7 },
+	{ id: 13, equipo: 'negro', 
+		nombre: 'Jeisson Linares', puntos: 13 },
 
-	{ id: 15, equipo: 'negro',
-		nombre: 'Fabio Garcia', puntos: 9 },
+	{ id: 14, equipo: 'blanco', 
+		nombre: 'Walter Gomez', puntos: 16 },
 
-	{ id: 16, equipo: 'blanco',
-		nombre: 'Jhon Guzman', puntos: 7 },
+	{ id: 15, equipo: 'negro', 
+		nombre: 'CUPO DISPONIBLE', puntos: 0 },
+
+	{ id: 16, equipo: 'blanco', 
+		nombre: 'CUPO DISPONIBLE', puntos: 0 },
 ]
